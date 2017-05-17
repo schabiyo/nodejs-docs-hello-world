4866d3cd56d0b17d2924ac51304f36ece8907bf8
Sani Chabi Yo <chabiyosani@gmail.com>
2017-05-17 13:15:34 -0400

    initial ci scripts

--- ci/infra/provision-infra.sh
+++ ci/infra/provision-infra.sh
@@ -4,8 +4,6 @@ set -e
 az login --service-principal -u $service_principal_id -p $service_principal_secret --tenant $tenant_id
 az account set --subscription "$subscription_id"  &> /dev/null
 #Create th eRG is it does not exist
-az group create --name "$rg_name" --location "$rg_location"
-
 #Check for existing RG
 az group show $rg_name 1> /dev/null
 
