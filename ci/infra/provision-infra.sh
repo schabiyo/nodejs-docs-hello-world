93f5dd8e5857eef1f07dbd55016c5aca07c9481c
Sani Chabi Yo <chabiyosani@gmail.com>
2017-05-17 15:12:06 -0400

    update2

--- ci/infra/provision-infra.sh
+++ ci/infra/provision-infra.sh
@@ -1,26 +1,16 @@
 #!/bin/bash
 #set -e
-set -euo pipefail
+set +e
 
 az login --service-principal -u $service_principal_id -p $service_principal_secret --tenant $tenant_id
 az account set --subscription "$subscription_id"  &> /dev/null
 #Create th eRG is it does not exist
 #Check for existing RG
 
+set -x
+az group create --name $rg_name --location $rg_location 1> /dev/null
+
 set +e
-az group show -n $rg_name 1> /dev/null
-
-if [ $? != 0 ]; then
-	echo "Resource group with name" $rg_name "could not be found. Creating new resource group.."
-	set -e
-	(
-		set -x
-		az group create --name $rg_name --location $rg_location 1> /dev/null
-	)
-	else
-	echo "Using existing resource group..."
-fi
-
 #Start deployment
 echo "Starting deployment..."
 (
