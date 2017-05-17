1a685d7690919720179715e6f3482a4e7d2cfdc5
Sani Chabi Yo <chabiyosani@gmail.com>
2017-05-17 16:03:15 -0400

    autogenerate the deployment user

--- ci/app/deploy-app.sh
+++ ci/app/deploy-app.sh
@@ -3,7 +3,12 @@ set -e
 
 az login --service-principal -u $service_principal_id -p $service_principal_secret --tenant $tenant_id
 az account set --subscription "$subscription_id"  &> /dev/null
-#az appservice web deployment user set --user-name $deployment_username --password $deployment_password
+
+uuid=$(uuidgen)
+
+echo "uuid=${uuid}"
+
+az appservice web deployment user set --user-name $uuid --password $deployment_password
 
 #Configure local GIT deployment
 git_url=$(az appservice web source-control config-local-git --name $webapp_name --resource-group $rg_name --query url --output tsv)
@@ -11,7 +16,7 @@ git_url=$(az appservice web source-control config-local-git --name $webapp_name 
 echo $git_url
 
 #Add the Password in the URL
-result_string="${git_url/$deployment_username/$deployment_username:$deployment_password}"
+result_string="${git_url/$uuid/$uuid:$deployment_password}"
 
 echo $result_string
 
