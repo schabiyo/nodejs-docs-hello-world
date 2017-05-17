580a1377ec02427fb248ab336d37a64f2548955d
Sani Chabi Yo <chabiyosani@gmail.com>
2017-05-17 15:26:22 -0400

    remove user duplication

--- ci/app/deploy-app.sh
+++ ci/app/deploy-app.sh
@@ -3,7 +3,7 @@ set -e
 
 az login --service-principal -u $service_principal_id -p $service_principal_secret --tenant $tenant_id
 az account set --subscription "$subscription_id"  &> /dev/null
-az appservice web deployment user set --user-name $deployment_username --password $deployment_password
+#az appservice web deployment user set --user-name $deployment_username --password $deployment_password
 
 #Configure local GIT deployment
 git_url=$(az appservice web source-control config-local-git --name $webapp_name --resource-group $rg_name --query url --output tsv)
